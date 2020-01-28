6c369ce8c15d832999eb5360445378b8a375b53a
兴杰 严 <keatkeat87@live.com>
2020-01-28 17:12:37 +0800

    ok

---
+++ Project/Entity/ApplicateionDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; } // nullable
+    }
+
+    public class CustomerOrderContext : DbContext
+    {
+        private readonly ILoggerFactory LoggerFactory;
+
+        public CustomerOrderContext(
+            DbContextOptions<CustomerOrderContext> options,
+            ILoggerFactory loggerFactory
+        )
+            : base(options)
+        {
+            LoggerFactory = loggerFactory;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseLoggerFactory(LoggerFactory);
+        }
+
+        public DbSet<Product> Products { get; set; }
+ 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(p => p.name).IsRequired(false).HasMaxLength(128);
+        }
+    }
+}
---
+++ Project/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Project.Entity;
+
+namespace Project.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet([FromServices] CustomerOrderContext Db)
+        {
+            Db.Products.Add(new Product
+            {
+                name = "mk100",
+            });
+            Db.Products.Add(new Product
+            {
+                name = "mk200",
+            });
+            Db.Products.Add(new Product
+            {
+                name = "mk300",
+            });
+            Db.Products.Add(new Product
+            {
+                name = null,
+            });
+            Db.Products.Add(new Product
+            {
+                name = null,
+            });
+
+            Db.SaveChanges();
+        }
+    }
+}
